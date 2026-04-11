9cb37e6a48096f10a7ed9aa2817087f1b3f9c71c
Mateusz Staroń <garneczek3@gmail.com>
2026-04-11 14:45:23 +0200

    change tool names

--- kubejs/client_scripts/main.js
+++ kubejs/client_scripts/main.js
@@ -1,3 +1,31 @@
 // Visit the wiki for more info - https://kubejs.com/
-console.info('Hello, World! (Loaded client example script)')
+ClientEvents.lang('en_us', event => {
+    // Tier I: Andesite Alloy
+    event.renameItem('minecraft:stone_pickaxe', 'Andesite Alloy Pickaxe')
+    event.renameItem('minecraft:stone_axe', 'Andesite Alloy Axe')
+    event.renameItem('minecraft:stone_shovel', 'Andesite Alloy Shovel')
+    event.renameItem('minecraft:stone_hoe', 'Andesite Alloy Hoe')
+    event.renameItem('minecraft:stone_sword', 'Andesite Alloy Sword')
+
+    // Tier II: Copper (Assuming modded IDs exist)
+    event.renameItem('minecraft:copper_pickaxe', 'Reinforced Copper Pickaxe')
+    event.renameItem('minecraft:copper_axe', 'Reinforced Copper Axe')
+    event.renameItem('minecraft:copper_shovel', 'Reinforced Copper Shovel')
+    event.renameItem('minecraft:copper_hoe', 'Reinforced Copper Hoe')
+    event.renameItem('minecraft:copper_sword', 'Reinforced Copper Sword')
+
+    // Tier IIb: Brass (Repurposing Gold)
+    event.renameItem('minecraft:golden_pickaxe', 'Brass Pickaxe')
+    event.renameItem('minecraft:golden_axe', 'Brass Axe')
+    event.renameItem('minecraft:golden_shovel', 'Brass Shovel')
+    event.renameItem('minecraft:golden_hoe', 'Brass Hoe')
+    event.renameItem('minecraft:golden_sword', 'Brass Sword')
+
+    // Tier III: Brass-Netherite
+    event.renameItem('minecraft:netherite_pickaxe', 'Brass-Netherite Pickaxe')
+    event.renameItem('minecraft:netherite_axe', 'Brass-Netherite Axe')
+    event.renameItem('minecraft:netherite_shovel', 'Brass-Netherite Shovel')
+    event.renameItem('minecraft:netherite_hoe', 'Brass-Netherite Hoe')
+    event.renameItem('minecraft:netherite_sword', 'Brass-Netherite Sword')
+})
 
